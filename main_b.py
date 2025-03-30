7d3495cda8c0087115659ded647fe531a3f7082a
karlstedt020 <karlstedt20148@gmail.com>
2025-03-30 15:49:49 +0300

    Applying adaptive block size

--- main_b.py
+++ main_b.py
@@ -24,6 +24,7 @@ def read_mit(name):
     return np.transpose(signals)
 
 NAME = 'Rh10010.edf'
+NAME2 = 'rec_1'
 N = 32
 M = 3
 
@@ -41,7 +42,8 @@ for arr in chebyshev_matrix:
 chebyshev_matrix = ls
 print("Chebyshev matrix ready")
 
-record_1 = read_mit("rec_1")
+#record_1 = read_mit(NAME2)
+record_1 = read_edf(NAME)
 f = ctypes.CDLL("./libtest.so").solve_c
 f.restype = ctypes.POINTER(ctypes.c_double)
 f.argtypes = (ctypes.POINTER(ctypes.c_double),ctypes.POINTER(ctypes.c_double),ctypes.c_int,ctypes.c_int)
@@ -56,6 +58,8 @@ total_avg_err = 0
 deviants = list()
 total_mse = 0
 total_mape = 0
+total_M = 0
+total_N = 0
 reconstructed = []
 for batch_i in range(0, measures, N):
     if batch_i+N > 5000:
@@ -64,20 +68,29 @@ for batch_i in range(0, measures, N):
         print(batch_i)
     mean = record_1[0, batch_i:batch_i+N].mean()
     slice_ = record_1[0, batch_i:batch_i+N].copy()
-    min_ = slice_.min()
-    max_ = slice_.max()
-    #if not np.allclose([max_ - min_], [0]):
-        #slice_ = (slice_ - min_) / (max_ - min_)
+    min_1 = slice_.min()
+    max_1 = slice_.max()
+    if abs(min_1 - max_1) > 1.:
+        print(max_1, min_1)
+        currM = N // 2
+        print("UES")
+    else:
+        currM = M
+    total_M += currM
+    total_N += N
+    if not np.allclose([max_1 - min_1], [0]):
+        slice_ = (slice_ - min_1) / (max_1 - min_1)
     numbers = numbers_array(*list(slice_))
     cheb = chebyshev_array(*chebyshev_matrix)
     start = datetime.now()
-    res = f(numbers, cheb, ctypes.c_int(N), ctypes.c_int(M))
+    res = f(numbers, cheb, ctypes.c_int(N), ctypes.c_int(currM))
     end = datetime.now()
     indeces = []
     for i in range(N):
         indeces.append(res[i])
     indeces = np.array(indeces)
     #indeces = indeces * (max_ - min_) + min_
+    indeces = indeces * (max_1 - min_1) + min_1
     reconstructed.extend(indeces)
     print("Batch", batch_i // 32, "MAPE:", mape_res := mape(y_true=record_1[0, batch_i:batch_i+N], y_pred=indeces), "MSE:", mse_res := mse(y_true=record_1[0, batch_i:batch_i+N], y_pred=indeces))
     #if mape_res >= 20:
@@ -97,6 +110,8 @@ avg_time = total_time / (measures // N)
 print("Average batch time:", avg_time)
 print("Average MAPE:", total_mape / (measures // N))
 print("Average MSE:", total_mse / (measures // N))
+print("Total N:", total_N, "")
+print("Total M:", total_M, "")
 fig, ax = plt.subplots()
 ln = len(reconstructed)
 ax.plot(np.arange(ln), record_1[0, :ln], linestyle='-', label="Original signal")
